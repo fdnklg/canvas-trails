bd2dccb444586fd8dec8b004afabc99e7db192f1
fdnklg <fabian.dinklage@me.com>
2019-04-13 14:36:01 +0200

    canvas animation barebone created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VectorCanvas :data="randomData" :config="config" :factor="isRetina"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import VectorCanvas from './components/VectorCanvas.vue';
+	import { range as d3Range } from 'd3';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	const conf = {
+		'width': 900,
+		'height': 600,
+		'pointsWidth': 6,
+		'pointsMargin': 4,
+	}
+
+	export default {
+	name: 'app',
+	components: {
+		VectorCanvas
+	},
+	computed: {
+		isRetina() {
+			return (window.devicePixelRatio > 1 ? 2 : 1);
+		},
+		config() {
+			return conf;
+		},
+		randomData() {
+			let arr = [];
+			d3Range(5000).forEach((index) => {
+				arr.push({
+					value: Math.random(),
+					index: index 
+				})
+			});
+
+			const arrPos = this.gridLayout(arr, conf.pointsWidth + conf.pointsMargin, conf.width, conf.height);
+			const arrNetwork = this.networks(arrPos);
+			return arrNetwork;
+		},
+	},
+	methods: {
+		gridLayout(points, pointsWidth, gridWidth, gridHeight) {
+			const pointsHeight = pointsWidth;
+			const pointsPerRow = Math.floor(gridWidth/pointsWidth);
+			const numRows = points.length / pointsPerRow;
+
+			points.forEach((point, i) => {
+				point.x = pointsWidth * (i % pointsPerRow);
+				point.y = pointsHeight * Math.floor(i / pointsPerRow);
+			});
+
+			return points;
+		},
+		networks(points) {
+			const numNetworks = 10;
+			const networkSize = 20;
+
+			let arr = [];
+
+			d3Range(numNetworks).forEach((index) => {
+				let randomPointStart = this.randomPoint(points);
+				let randomNetworkSize = Math.floor(networkSize * Math.random());
+
+				for (let index = 0; index <= randomNetworkSize; index++) {
+					let randomPointEnd = this.randomPoint(points);
+
+					arr.push({
+						startX: randomPointStart.x,
+						startY: randomPointStart.y,
+						endX: randomPointEnd.x,
+						endY: randomPointEnd.y,
+					});
+				}
+			});
+
+			return arr;
+		},
+		randomPoint(arr) {
+			const randomIndex = Math.floor(arr.length * Math.random());
+			return arr[randomIndex];
+		}
+	},
+	mounted() {
+	}
+	}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+	}
 </style>
---
+++ src/components/VectorCanvas.vue
@@ -0,0 +1,115 @@
+<template>
+    <div ref="vis" id="vis" class="vis-wrapper"></div>
+</template>
+
+<script>
+    import { 
+        select as d3Select,
+        timer as d3Timer,
+        easeCubic as d3EaseCubic
+    } from 'd3';
+    export default {
+        name: 'VectorCanvas',
+        props: ['data', 'config', 'factor'],
+        data() {
+            return {
+                canvas: null,
+                width: this.config.width,
+                height: this.config.height,
+                pointsWidth: this.config.pointsWidth,
+                context: null,
+                animAge: 0,
+                maxAge: 100,
+                frameRate: 30,
+                age: [],
+                duration: 3000,
+                ease: null
+            }
+        },
+        methods: {
+            init() {
+                this.canvas = d3Select('#vis').append('canvas')
+                    .classed('vis-canvas', true)
+                    .attr('width', this.width * this.factor)
+                    .attr('height', this.height * this.factor)
+
+                this.canvas
+                    .attr('style', `width: ${this.width}px; height: ${this.height}px`)
+
+                this.context = this.canvas.node().getContext('2d');
+                this.context.save();
+                this.context.clearRect(0, 0, this.width, this.height);
+                this.context.scale(this.factor, this.factor);
+
+                this.context.fillStyle = "rgba(0, 0, 0, 0.5)";
+                this.context.strokeStyle = '#FF8000';
+                this.context.lineWidth = 2;
+            },
+            animationInit() {
+                for (var i = 0; i < this.data.length; i++) {
+                    this.age.push(this.randomAge());
+                };
+
+                this.context.globalCompositeOperation = "source-over";
+
+                this.ease = d3EaseCubic;
+
+                let timer = d3Timer((elapsed) => {
+                    const t = Math.min(1, this.ease(elapsed / this.duration));
+
+                    this.data.forEach((point,i) => {
+                        point.x = point.startX * (1 - t) + point.endX * t;
+                        point.y = point.startY * (1 - t) + point.endY * t;
+                    });
+
+                    if (t === 1) {
+                        // stop this timer since we are done animating.
+                        timer.stop();
+                    }
+
+                    this.drawPoints()
+                })
+            },
+            drawPoints() {
+                this.context.fillStyle = 'rgba(255,255,255,.1)';
+                this.context.fillRect(0, 0, this.width, this.height);
+                
+                this.data.forEach((point,i) => {
+                    this.context.fillStyle = '#FF8000';
+                    this.context.fillRect(point.x, point.y, this.pointsWidth, this.pointsWidth);
+                })
+            },
+            drawLines() {
+                this.context.fillRect(0, 0, this.width, this.height);
+ 
+                this.data.forEach((item, i) => {
+                    this.context.beginPath();
+                    this.context.moveTo(item.startX, item.startY);
+                    this.context.lineTo(item.endX, item.endY);
+                    this.context.stroke();
+
+                    if (this.age[i]++ > this.maxAge) {
+                        this.age[i] = this.randomAge();
+                    }
+                })
+
+
+            },
+            randomAge() {
+                return Math.round(Math.random() * 100);
+            }
+        },
+        mounted() {
+            this.init();
+            this.animationInit();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .vis-wrapper {
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
